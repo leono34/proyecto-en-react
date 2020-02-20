1b5dc20082bce87057e02cf7721c914562791bf6
Joan Macha <isaelmacha@hotmail.com>
2020-02-20 14:05:26 -0500

    header

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <header>
+        <a href="#" className="logo">Logo</a>
+        <div className="menu-toggle"></div>
+          <nav>
+            <ul>
+              <li><a href="#" className="active" >Inicio</a></li>
+              <li><a href="#">Sobre Nosotros</a></li>
+              <li><a href="#">Servicios</a></li>
+              <li><a href="#">Paseadores</a></li>
+              <li><a href="#">Registrate</a></li>
+            </ul>
+          </nav>
+          <div className="main" ></div>
+    </header>
+  )
+}
+
+export {
+  Header
+}

35f6fe11ebdb6d591b7587344bf5d5912ac8c077
unknown <paolo.minaya.c@gmail.com>
2020-02-24 16:49:45 -0500

    ordenando sass

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -15,7 +15,7 @@ import {
 
 
 function Footer() {
-  const github = 'https://github.com/luicsistem/petwalker';
+  const github = 'https://github.com/leono34/proyecto-en-react.git';
   const facebook = '';
   const instagram = '';
   const youtube = '';
@@ -23,57 +23,52 @@ function Footer() {
   return (
     <footer className="footer">
 
-      <div className="row">
+      <div className=" d-flex justify-content-between" >
 
-          <div className="col-md-4">
-            <h3 className="text-danger">About PetWalker</h3>
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <div className="col-md-4 text-center">
+            <h3 className="text-danger ">Nosotros</h3>
+            <p>Petwalker es la manera más sencilla que tienen los propietarios de mascotas para encontrar un cuidador de confianza en su zona.</p>
           </div>
 
-          <div className="col-md-4">
-            <h3 className="text-danger">Subscribe</h3>
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+          <div className="col-md-4 text-center">
+            <h3 className="text-danger text-center">Suscribirse</h3>
+            <p>Cada servicio Petwalker que ofrece está cubierto por la Garantía Petwalker.
             </p>
        
           </div>
 
-          <div className="col-md-4">
-            <h3 className="text-danger">Contact Info</h3>
-            <div className="footer-contact-info-wrap">
+          <div className="col-md-4 text-center">
+            <h3 className="text-danger">Contactanos</h3>
                 <ul className="list-group">
                     <li>
-                        <span>
+                        <p>
                         Calle Boston 220 Las Vegas 
-                        </span>
+                        </p>
                     </li>
                     <li>
                         <i className="fa fa-phone"></i>
-                        <span>51-123 456 78 9</span>
+                        <p>
+                        51-123 456 78 9
+                        </p>
                     </li>
                     <li>
-                        
                         <i className="fa fa-envelope"></i>
-                        <span>petwalker@gmail.com</span>
+                        <p>
+                        petwalker@gmail.com
+                        </p>
                     </li>
                 </ul>
-            </div>
           </div>
-
-
       </div>
 
-      <FontAwesomeIcon/>
-
-
-      <div>
-      <ul>
-        <li><a href={facebook} target="_blank"><FontAwesomeIcon icon={faFacebook} /></a></li>
-        <li><a href={instagram} target="_blank"><FontAwesomeIcon icon={faInstagram} /></a></li>
-        <li><a href={github} target="_blank"><FontAwesomeIcon icon={faGithub} /></a></li>
-        <li><a href={youtube} target="_blank"><FontAwesomeIcon icon={faYoutube} /></a></li>
-      </ul>
-      <p className="copyright">Copyright 2020 | <b>Petwalker</b></p>
-
+      <div className="text-center">
+       <ul>
+         <li ><a  href={facebook} target="_blank"><FontAwesomeIcon className="fa-xs" icon={faFacebook} /></a></li>
+         <li><a href={instagram} target="_blank"><FontAwesomeIcon className="fa-xs" icon={faInstagram} /></a></li>
+         <li><a href={github} target="_blank"><FontAwesomeIcon className="fa-xs" icon={faGithub} /></a></li>
+         <li><a href={youtube} target="_blank"><FontAwesomeIcon className="fa-xs" icon={faYoutube} /></a></li>
+       </ul>
+       <p className="copyright">Copyright 2020 | <b>Petwalker</b></p>
       </div>
     </footer>
   )

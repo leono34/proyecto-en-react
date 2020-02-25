8511e9484edc1e3ee77154e21d146e2f4f580fad
Joan Macha <isaelmacha@hotmail.com>
2020-02-25 16:05:42 -0500

    dashboard cliente

--- src/features/client/components/Client.dashboard.jsx
+++ src/features/client/components/Client.dashboard.jsx
@@ -11,22 +11,22 @@ class Dueño extends React.Component {
         <div className="dueño">
           <div className="row">
             <div className="col-md-12">
-              <div className="row">
-              <div className="col-md-4">
-                <div className="row">
-                  <div className="col-md-12 cuerpo">
-                    <img className="img" src="" alt=""/>
-                    <p>soy el dueño </p>
-                    <hola/>
-                  </div>
-                  <div className="col-md-12 cuerpo">
-                    <img src="" alt=""/>
-                    <p>soy el dueño1 </p>
-                    <hola/>
+              <div className="row separar">
+                <div className="col-md-4 cuerpohe">
+                  <div className="row">
+                    <div className="col-md-12 cuerpo">
+                      <img className="img" src="" alt=""/>
+                      <p>soy el dueño </p>
+                      <hola/>
+                    </div>
+                    <div className="col-md-12 cuerpo">
+                      <img src="" alt=""/>
+                      <p>soy el dueño1 </p>
+                      <hola/>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-md-8 card">
+                <div className="col-md-7 card">
                 <div className="card-invitacion">
                    <h5>recomienda a un amigo, Gana S/20</h5>
                    <p>por cada amigo que reserve su estadia ,le daremos un credito de S/20 para su proxima 
@@ -34,7 +34,7 @@ class Dueño extends React.Component {
                      <button className="btn btn-warning">invita a un amigo</button>
                 </div>
               </div>
-            </div>
+              </div>
             </div>
           </div>
         </div>
